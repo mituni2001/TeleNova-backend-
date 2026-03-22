e147b74a590a6dbe9a07bb6a15d4adb5d96633e1
Mithuni Dinesha <matheeshasandeep96@gmail.com>
2026-03-22 19:45:49 +0530

    Temp rename batteryBank.controller for Linux

--- routes/batterybank.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createBatteryBank, getBatteryBanks } = require("../controllers/batteryBank.controller");
-
-router.post("/", createBatteryBank);
-router.get("/", getBatteryBanks);
-
-module.exports = router;
